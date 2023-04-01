570bd487f4886125b9113f45f74afb1ddb27a5c2
Magdalena <kramekm95@gmail.com>
2023-04-01 21:12:47 +0200

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\CommentsController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/comments',[CommentsController::class,'index']);
+Route::get('/comments', [CommentsController::class, 'index'])->name('comments');
+Route::get('/create', [CommentsController::class, 'create'])->name('create');
+Route::post('/store', [CommentsController::class, 'store'])->name('store');
+Route::get('/delete/{id}', [CommentsController::class, 'destroy'])->name('delete');
+Route::get('/edit/{id}', [CommentsController::class,'edit'])->name('edit');
+Route::put('/update/{id}', [CommentsController::class,'update'])->name('update');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
